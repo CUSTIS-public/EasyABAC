02a0965fc31873149d034ba07720357386626b28
esedov <v8sb9cuxc>
2018-09-19 17:33:52 +0300

    Bug 259906 - Разработка фреймворка для доклада о технической реализации ABAC

--- easyabac-core/src/main/java/ru/custis/easyabac/core/PolicyInitializer.java
+++ easyabac-core/src/main/java/ru/custis/easyabac/core/PolicyInitializer.java
@@ -14,40 +14,10 @@ import java.util.Set;
 
 public class PolicyInitializer {
 
-
-//    private static void initBalana() {
-//
-//        try {
-//            // using file based policy repository. so set the policy location as system property
-//            String policyLocation = (new File(".")).getCanonicalPath() + File.separator + "resources" + File.separator + "policy.xml";
-//            System.setProperty(FileBasedPolicyFinderModule.POLICY_DIR_PROPERTY, policyLocation);
-//        } catch (IOException e) {
-//            System.err.println("Can not locate policy repository");
-//        }
-//        // create default instance of Balana
-//        balana = Balana.getInstance();
-//    }
-
-    /**
-     * Returns a new PDP instance with new XACML policies
-     *
-     * @return a  PDP instance
-     */
     public PDP getPDPNewInstance(String policyXacml) {
 
         PolicyFinder policyFinder = new PolicyFinder();
-
-//        Set<String> policyLocations = new HashSet<>();
-//        try {
-//            String policyPath = (new File(".")).getCanonicalPath() + File.separator +
-//                    "src" + File.separator + "test" + File.separator +
-//                    "resources" + File.separator + "policy.xml";
-//            policyLocations.add(policyPath);
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-
-//        FileBasedPolicyFinderModule fileBasedPolicyFinderModule = new FileBasedPolicyFinderModule(policyLocations);
+        
         PolicyFinderModule stringPolicyFinderModule = new StringPolicyFinderModule(policyXacml);
         Set<PolicyFinderModule> policyModules = new HashSet<>();
 
