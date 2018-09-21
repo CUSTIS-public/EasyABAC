431ee3e3fc8f561c47b8f420afe5947cd9a23217
esedov <v8sb9cuxc>
2018-09-21 17:45:21 +0300

    Bug 259906 - Разработка фреймворка для доклада о технической реализации ABAC Модель политики

--- easyabac-core/src/main/java/ru/custis/easyabac/core/models/attribute/Attribute.java
+++ easyabac-core/src/main/java/ru/custis/easyabac/core/models/attribute/Attribute.java
@@ -8,4 +8,44 @@ public class Attribute {
     private DataType dataType;
     private boolean array;
     private Set<String> allowableValues;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public DataType getDataType() {
+        return dataType;
+    }
+
+    public void setDataType(DataType dataType) {
+        this.dataType = dataType;
+    }
+
+    public boolean isArray() {
+        return array;
+    }
+
+    public void setArray(boolean array) {
+        this.array = array;
+    }
+
+    public Set<String> getAllowableValues() {
+        return allowableValues;
+    }
+
+    public void setAllowableValues(Set<String> allowableValues) {
+        this.allowableValues = allowableValues;
+    }
 }
