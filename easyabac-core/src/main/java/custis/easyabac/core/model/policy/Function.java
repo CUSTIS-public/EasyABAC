cb64a6c8cb4b6ec218f7f206041ff66a69f1fcc8
Viacheslav Muravlev <vmuravlev@gmail.com>
2018-09-30 18:44:03 +0300

    #1 Подключен JavaCC, грамматика для разбора выражений в политиках

--- easyabac-core/src/main/java/custis/easyabac/core/model/policy/Function.java
+++ easyabac-core/src/main/java/custis/easyabac/core/model/policy/Function.java
@@ -4,15 +4,19 @@ public enum Function {
     EQUAL("=="),
     GREATER(">"),
     LESS("<"),
-    GREATER_OR_EQUEL(">="),
-    LESS_OR_EQUEL("<="),
+    GREATER_OR_EQUAL(">="),
+    LESS_OR_EQUAL("<="),
     IN("in"),
     ONE_OF("one-of"),
     SUBSET("subset");
+
     private String functionName;
 
     Function(String functionName) {
         this.functionName = functionName;
     }
 
+    public String getFunctionName() {
+        return functionName;
+    }
 }
---
+++ easyabac-core/src/test/java/custis/easyabac/PolicyExpressionParserTest.java
@@ -0,0 +1,19 @@
+package custis.easyabac;
+
+import org.junit.Test;
+
+//import custis.easyabac.core.model.policy.PolicyExpressionParser;
+
+/**
+ * Test suite for various policy expressions parsing into EasyABAC domain model
+ */
+public class PolicyExpressionParserTest {
+
+//    private PolicyExpressionParser parser;
+    
+    @Test
+    public void shouldReadActionIdEqualsExpression() {
+
+    }
+
+}
