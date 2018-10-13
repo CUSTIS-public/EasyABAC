2655a44c16c10cd82fab015d69706eb03ce4d5e0
vmuravlev <vmuravlev@custis.ru>
2018-10-13 13:27:30 +0300

    #1 Вместо Map<URI, Policy> собирается PolicySet

--- easyabac-core/src/main/java/custis/easyabac/core/init/BalanaModelTransformer.java
+++ easyabac-core/src/main/java/custis/easyabac/core/init/BalanaModelTransformer.java
@@ -4,9 +4,13 @@ import java.net.URI;
 
 public class BalanaModelTransformer {
     private static final String POLICY_NAMESPACE = "urn:oasis:names:tc:xacml:3.0:easy-policy";
+    private static final String DEFAULT_POLICY_SET_ID = "urn:oasis:names:tc:xacml:3.0:easy-policy:policy-set";
 
     public static URI balanaPolicyId(String id) {
         return URI.create(POLICY_NAMESPACE + ":" + id);
+    }
+    public static URI defaultBalanaPolicySetId() {
+        return URI.create(DEFAULT_POLICY_SET_ID);
     }
 
     public static URI balanaRuleId(String policyId, String id) {
