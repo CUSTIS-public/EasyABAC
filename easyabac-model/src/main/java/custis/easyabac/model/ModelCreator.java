bc19707fd4a6c8e802d6a1c581228bb7bd63b883
vmuravlev <vmuravlev@custis.ru>
2018-10-19 00:46:03 +0300

    #25 Доработан конвертер, сделана командная оболочка

--- easyabac-model/src/main/java/custis/easyabac/model/ModelCreator.java
+++ easyabac-model/src/main/java/custis/easyabac/model/ModelCreator.java
@@ -1,7 +1,9 @@
 package custis.easyabac.model;
 
 import java.io.InputStream;
+import java.io.Reader;
 
 public interface ModelCreator {
     AbacAuthModel createModel(InputStream stream) throws EasyAbacInitException;
+    AbacAuthModel createModel(Reader reader) throws EasyAbacInitException;
 }
--- easyabac-tools/easy-model-xacml-converter/src/main/java/custis/easyabac/model/xacmlconverter/EasyModel2XACMLConverter.java
+++ easyabac-tools/easy-model-xacml-converter/src/main/java/custis/easyabac/model/xacmlconverter/EasyModel2XACMLConverter.java
@@ -2,6 +2,11 @@ package custis.easyabac.model.xacmlconverter;
 
 import custis.easyabac.core.pdp.balana.BalanaPolicyBuilder;
 import custis.easyabac.model.AbacAuthModel;
+import custis.easyabac.model.EasyAbacInitException;
+import custis.easyabac.model.easy.EasyAbacModelCreator;
+
+import java.io.InputStream;
+import java.io.Reader;
 
 public class EasyModel2XACMLConverter {
     private BalanaPolicyBuilder balanaPolicyBuilder;
@@ -10,7 +15,21 @@ public class EasyModel2XACMLConverter {
         this.balanaPolicyBuilder = new BalanaPolicyBuilder();
     }
 
-    public String convert(AbacAuthModel authModel) {
-        return balanaPolicyBuilder.buildFrom(authModel).encode();
+    public String convert(InputStream inputStream) throws ConversionException {
+        try {
+            AbacAuthModel model = new EasyAbacModelCreator().createModel(inputStream);
+            return this.balanaPolicyBuilder.buildFrom(model).encode();
+        } catch (EasyAbacInitException e) {
+            throw new ConversionException("Failed to create EasyModel from InputStream", e);
+        }
+    }
+
+    public String convert(Reader reader) throws ConversionException {
+        try {
+            AbacAuthModel model = new EasyAbacModelCreator().createModel(reader);
+            return this.balanaPolicyBuilder.buildFrom(model).encode();
+        } catch (EasyAbacInitException e) {
+            throw new ConversionException("Failed to create EasyModel from Reader", e);
+        }
     }
 }
